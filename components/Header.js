8c8147013242c17b17cde208aa0703d7bbe94437
deepanshu <deepanshudabas2003@gmail.com>
2023-08-12 21:21:32 +0530

    all

--- components/Header.js
+++ components/Header.js
@@ -34,9 +34,9 @@ function NavBar() {
     <header className="bg-black">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
-            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+            <img className="h-5 w-auto" src="/flipkart.svg/" alt="" />
           </a>
         </div>
         <div className="flex lg:hidden">
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import "@/styles/stylesfooter.css";
-
+import Head from 'next/head'
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico/" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
