647699aca2901ae84ffc1fc4fcabe08dc4c1eedf
SilmeyButter <95627241+divyansh10108@users.noreply.github.com>
2023-08-13 20:16:30 +0530

    Update SignIn.js

--- components/SignIn.js
+++ components/SignIn.js
@@ -1,87 +1,97 @@
+import React, { useState } from 'react';
+import Papa from 'papaparse';
+import { useRouter } from 'next/router';
+import { setCookie } from 'nookies';
+import Typewriter from 'typewriter-effect';
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+  
 
-export default function Forms() {
-    return (
-      <>
-        {/*
-          This Forms requires updating your template:
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch('/MainReviews.csv');
+    const text = await response.text();
+    const { data } = Papa.parse(text, { header: true });
+    console.log(response)
+    // Verify credentials
+    const user = data.find((entry) => entry.UserId === username); // Change this line
+    console.log(user)
+    // if (user) { // Make sure to add Password field to your CSV or change the field name
+    //   // Set a session cookie
+    //   setCookie(null, 'user', JSON.stringify(user), {
+    //     maxAge: 3600, // Cookie expires in 1 hour
+    //     path: '/',
+    //   });
+
+    //   router.push('/dashboard'); // Redirect to the dashboard or protected page
+    // } else {
+      
+    //   alert('Invalid username or password.');
+    // }
+  };
   
-          ```
-          <html class="h-full bg-white">
-          <body class="h-full">
-          ```
-        */}
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              className="mx-auto h-10 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
-            />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white-900">
-              Sign in to your account
-            </h2>
-          </div>
-  
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" action="#" method="POST">
-              <div>
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-white-900">
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-white-900 shadow-sm ring-1 ring-inset ring-white-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-white-900">
-                    Password
-                  </label>
-                  <div className="text-sm">
-                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                      Forgot password?
-                    </a>
-                  </div>
-                </div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-white-900 shadow-sm ring-1 ring-inset ring-white-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign in
-                </button>
-              </div>
-            </form>
-  
-            <p className="mt-10 text-center text-sm text-white-500">
-              Not a member?{' '}
-              <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                Start a 14 day free trial
-              </a>
-            </p>
-          </div>
+
+  return (
+    <div className="flex h-screen">
+      {/* Left Side */}
+      
+      <div className="w-2/3 bg-blue-700 text-white p-12 flex flex-col justify-center items-center">
+        <div className="text-4xl font-bold mb-4">
+        <Typewriter 
+          options={{
+            strings: ["Internship", 'Lagwado'],
+            autoStart: true,
+            loop: true,
+          }}
+        />
         </div>
-      </>
-    )
-  }
-  
+        <p className="text-lg text-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor bibendum est,
+          ultrices sodales enim efficitur id.
+        </p>
+      </div>
+      
+      {/* Right Side */}
+      <div className="w-1/3 flex justify-center items-center">
+        <form onSubmit={handleSubmit} className="grid grid-cols-1">
+          <div className="mb-4">
+            <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              className="block w-full rounded-md border-0 py-1.5 text-white-900 shadow-sm ring-1 ring-inset ring-white-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="block w-full rounded-md border-0 py-1.5 text-white-900 shadow-sm ring-1 ring-inset ring-white-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="bg-blue-600 hover:bg-black hover:text-blue-500 text-white font-bold py-2 px-6 rounded-lg focus:shadow-outline"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
