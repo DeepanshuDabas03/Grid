30bc52277f00bc16abdc34235d9974f245175f95
SilmeyButter <95627241+divyansh10108@users.noreply.github.com>
2023-08-15 21:40:35 +0000

    integrated with new dataset

--- components/collections.js
+++ components/collections.js
@@ -12,9 +12,13 @@ export default function Home() {
   useEffect(() => {
     // Load and parse CSV data
     async function loadCSVData() {
-      const response = await fetch('/Feviews2.csv'); // Adjust the file path
-      const text = await response.text();
-      setCSVData(Papa.parse(text, { header: true, dynamicTyping: true }).data);
+      try {
+        const response = await fetch('/OrderHistory.csv');
+        const text = await response.text();
+        setCSVData(Papa.parse(text, { header: true, dynamicTyping: true }).data);
+      } catch (error) {
+        console.error('Error loading CSV data:', error);
+      }
     }
     loadCSVData();
   }, []);
@@ -22,21 +26,52 @@ export default function Home() {
   useEffect(() => {
     // Retrieve the userId from the cookie
     const userIdFromCookie = Cookies.get('myCookie');
-    
+    console.log(userIdFromCookie)
     // Fetch data from the JSON file in the public folder
-    fetch('/top_n.json')
+    fetch('/formatted_top_n.json')
       .then((response) => response.json())
       .then((data) => {
         // Filter data based on the userId
         const userTopN = data[userIdFromCookie] || [];
-        setUserTopNData(userTopN);
+        console.log(userTopN)
+        // Fetch product information from ProductsList.csv based on ProductId
+        fetch('/ProductsList.csv')
+          .then((response) => response.text())
+          .then((csvText) => {
+            // console.log('Parsed CSV Data:', csvText);
+            const parsedCSV = Papa.parse(csvText, { header: true, dynamicTyping: true }).data;
+            // console.log('Parsed CSV:', parsedCSV);
+            const userTopNWithProductInfo = userTopN.map(({ ProductId, Rating }) => {
+              let productInfo = null;
+            
+              // Find productInfo using ProductId in parsedCSV
+              for (let i = 0; i < parsedCSV.length; i++) {
+                if (parsedCSV[i].ProductId == ProductId) {
+                  productInfo = parsedCSV[i];
+                  break;
+                }
+              }
+            
+              console.log('Product Info:', productInfo);
+            
+              const productName = productInfo ? productInfo.product_name : 'Unknown Product';
+              const productImage = productInfo ? productInfo.product_url : '';
+              return { ProductId, Rating, productName, productImage };
+            });
+            
+            setUserTopNData(userTopNWithProductInfo);
+            
+          })
+          
+          
+          .catch((error) => console.error('Error fetching ProductsList.csv:', error));
       })
-      .catch((error) => console.error('Error fetching data:', error));
+      .catch((error) => console.error('Error fetching top_n.json:', error));
   }, []);
 
   function getProductName(productId) {
     const product = csvData.find((item) => item.ProductId === productId);
-    return product ? product.ProductName : 'Unknown Product';
+    return product ? product.Product_name : 'Unknown Product';
   }
 
   function getProductInfo(productId) {
@@ -77,16 +112,22 @@ export default function Home() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h1 className="text-3xl font-bold text-white mb-8">Top N Recommendations</h1>
         <Slider {...settings}>
-          {userTopNData.map(([productId, rating]) => (
+          {userTopNData.map(({ productId, rating, productName, productImage }) => (
             <div key={productId} className="px-2">
               <div className="bg-blue-900 p-4 rounded-lg shadow-md">
-                <h3 className="text-lg font-semibold text-white mb-2">{getProductName(productId)}</h3>
-                <p className="text-gray-300">{getProductInfo(productId)}</p>
+                <h3 className="text-lg font-semibold text-white mb-2">{productName}</h3>
+                <img
+                  src={productImage}
+                  alt={productName}
+                  className="mb-2"
+                  style={{ maxHeight: '150px', objectFit: 'cover' }}
+                />
                 <p className="font-bold mt-2 text-white">Rating: {rating}</p>
                 <div className="mt-4 space-x-2">
                   <button className="px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-700">
                     View
                   </button>
+
                   <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-400">
                     Add to Cart
                   </button>
@@ -99,89 +140,3 @@ export default function Home() {
     </div>
   );
 }
-
-
-
-
-
-
-
-
-// import { useRouter } from 'next/router';
-// import Cookies from 'js-cookie';
-
-// const callouts = [
-//   {
-//     name: "Desk and Office",
-//     description: "Work from home accessories",
-//     imageSrc:
-//       "https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg",
-//     imageAlt:
-//       "Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.",
-//     href: "#",
-//   },
-//   {
-//     name: "Self-Improvement",
-//     description: "Journals and note-taking",
-//     imageSrc:
-//       "https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg",
-//     imageAlt:
-//       "Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.",
-//     href: "#",
-//   },
-//   {
-//     name: "Travel",
-//     description: "Daily commute essentials",
-//     imageSrc:
-//       "https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg",
-//     imageAlt: "Collection of four insulated travel bottles on wooden shelf.",
-//     href: "#",
-//   },
-// ];
-
-// export default function Collections() {
-//   const router = useRouter();
-//     const authToken = Cookies.get('myCookie');
-//     console.log(authToken)
-//     // // Check authentication status
-//     const isAuthenticated = !!authToken;
-//     console.log(Cookies.get('myCookie')); // Add the missing closing parenthesis here
-//     console.log(isAuthenticated)
-//     if (!isAuthenticated) {
-//       // Redirect to the login page if not authenticated
-//       router.replace('/login');
-//       return null; // Return null to prevent rendering
-//     }
-//   return (
-//     <div className="bg-black-100">
-//       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-//         <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-//           <h2 className="text-2xl font-bold text-white-900">Collections</h2>
-
-//           <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
-//             {callouts.map((callout) => (
-//               <div key={callout.name} className="group relative">
-//                 <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-//                   <img
-//                     src={callout.imageSrc}
-//                     alt={callout.imageAlt}
-//                     className="h-full w-full object-cover object-center"
-//                   />
-//                 </div>
-//                 <h3 className="mt-6 text-sm text-white-500">
-//                   <a href={callout.href}>
-//                     <span className="absolute inset-0" />
-//                     {callout.name}
-//                   </a>
-//                 </h3>
-//                 <p className="text-base font-semibold text-white-900">
-//                   {callout.description}
-//                 </p>
-//               </div>
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
