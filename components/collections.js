1cf66c5f9503d81d2408a6915f60d9ae2d1fdaa6
deepanshu <deepanshudabas2003@gmail.com>
2023-08-18 22:35:10 +0530

    kuch toh hua xd

--- components/collections.js
+++ components/collections.js
@@ -4,6 +4,7 @@ import Cookies from "js-cookie";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Image from "next/image";
 
 export default function Home() {
   const [csvData, setCSVData] = useState([]);
@@ -26,61 +27,63 @@ export default function Home() {
     loadCSVData();
   }, []);
   useEffect(() => {
-    console.log("userTopNData:", userTopNData);
     setIsLoading(true);
   }, [userTopNData]);
   useEffect(() => {
     // Retrieve the userId from the cookie
     const userIdFromCookie = Cookies.get("myCookie");
-    console.log("userIdFromCookie:", userIdFromCookie);
-  
+
     // Fetch data from the JSON file in the public folder
     fetch("/formatted_top_n.json")
       .then((response) => response.json())
       .then(async (data) => {
-        console.log("JSON data:", data);
-  
         // Filter data based on the userId
         const userTopN = data[userIdFromCookie] || [];
-        console.log("userTopN:", userTopN);
-  
+
         // Fetch product information from ProductsList.csv based on ProductId
         const response = await fetch("/ProductsList.csv");
         const csvText = await response.text();
-        console.log("CSV text:", csvText);
-  
+
         const parsedCSV = Papa.parse(csvText, {
           header: true,
           dynamicTyping: true,
         }).data;
-        console.log("Parsed CSV:", parsedCSV);
-  
-        const userTopNWithProductInfo = userTopN.map(({ ProductId, Rating }) => {
-          let productInfo = null;
-  
-          // Find productInfo using ProductId in parsedCSV
-          for (let i = 0; i < parsedCSV.length; i++) {
-            if (parsedCSV[i].ProductId == ProductId) {
-              productInfo = parsedCSV[i];
-              break;
+
+        const userTopNWithProductInfo = userTopN.map(
+          ({ ProductId, Rating }) => {
+            let productInfo = null;
+
+            // Find productInfo using ProductId in parsedCSV
+            for (let i = 0; i < parsedCSV.length; i++) {
+              if (parsedCSV[i].ProductId == ProductId) {
+                productInfo = parsedCSV[i];
+                break;
+              }
             }
+            const productName = productInfo
+              ? productInfo.product_name
+              : "Unknown Product";
+            let productImage = productInfo ? productInfo.image : "";
+            const nn = productImage.split(",");
+            productImage = nn[0];
+            productImage= productImage.replace('"', "");
+            productImage= productImage.replace('"', "");
+            productImage= productImage.replace('[', "");
+            return { ProductId, Rating, productName, productImage };
           }
-          const productName = productInfo ? productInfo.product_name : "Unknown Product";
-          const productImage = productInfo ? productInfo.product_url : "";
-          return { ProductId, Rating, productName, productImage };
-        });
+        );
         setIsLoading(false);
         setUserTopNData(userTopNWithProductInfo);
       })
       .catch((error) => console.error("Error fetching top_n.json:", error));
   }, []);
 
- async function getProductName(ProductId) {
+  async function getProductName(ProductId) {
     const product = csvData.find((item) => item.ProductId === ProductId);
     return product ? product.Product_name : "Unknown Product";
   }
 
- async function getProductInfo(ProductId) {
+  async function getProductInfo(ProductId) {
     const product = csvData.find((item) => item.ProductId === ProductId);
     if (product) {
       return `${product.Brand} - ${product.ProductName}`;
@@ -90,7 +93,6 @@ export default function Home() {
   }
   async function fetchRecommendations(product_id) {
     try {
-      console.log("Product ID:", product_id);
       const response = await fetch("http://127.0.0.1:9010/predict", {
         method: "POST",
         headers: {
@@ -100,7 +102,6 @@ export default function Home() {
       });
 
       const data = await response.json();
-      console.log("Recommendations:", data);
       setRecommendedProducts(data); // Update the recommended products state
     } catch (error) {
       console.error("Error fetching recommendations:", error);
@@ -131,50 +132,49 @@ export default function Home() {
     ],
   };
 
-    console.log("userTopNData:", userTopNData);
   return (
-    
     <div className="bg-black py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h1 className="text-3xl font-bold text-white mb-8">
           Top N Recommendations
         </h1>
         {isLoading ? (
-          
-          
-        <Slider {...settings}>
-          {userTopNData
-            .map(({ ProductId, Rating, productName, productImage }) => (
-              
-              <div key={ProductId} className="px-2">
-                <div className="bg-blue-900 p-4 rounded-lg shadow-md">
-                  <h3 className="text-lg font-semibold text-white mb-2">
-                    {productName}{" "}
-                  </h3>
-                  <img
-                    src={productImage}
-                    alt={productName}
-                    className="mb-2"
-                    style={{ maxHeight: "150px", objectFit: "cover" }}
-                  />
-                  <p className="font-bold mt-2 text-white">Rating: {Rating}</p>
-                  <div className="mt-4 space-x-2">
-                    <button className="px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-700">
-                      View
-                    </button>
+          <Slider {...settings}>
+            {userTopNData.map(
+              ({ ProductId, Rating, productName, productImage }) => (
+                <div key={ProductId} className="px-2">
+                  <div className="bg-blue-900 p-4 rounded-lg shadow-md">
+                    <h3 className="text-lg font-semibold text-white mb-2">
+                      {productName}{" "}
+                    </h3>
+                    <Image
+                      src={productImage}
+                      alt={productName}
+                      className="mb-2"
+                      width={500}
+                      height={500}
+                      style={{ maxHeight: "150px", objectFit: "cover" }}
+                    />
+                    <p className="font-bold mt-2 text-white">
+                      Rating: {Rating}
+                    </p>
+                    <div className="mt-4 space-x-2">
+                      <button className="px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-700">
+                        View
+                      </button>
 
-                    <button
-                      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-400"
-                      onClick={() => fetchRecommendations(ProductId)} // Call fetchRecommendations on button click
-                    >
-                      Add to Cart
-                    </button>
+                      <button
+                        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-400"
+                        onClick={() => fetchRecommendations(ProductId)} // Call fetchRecommendations on button click
+                      >
+                        Add to Cart
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-        </Slider>
-        
+              )
+            )}
+          </Slider>
         ) : (
           <p className="text-white">Loading...</p>
         )}
