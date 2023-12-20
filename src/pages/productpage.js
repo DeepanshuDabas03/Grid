c7ef175e351eb0810cf25c4f2fca59206c8d46b8
DeepanshuDabas03 <deepanshu21249@iiitd.ac.in>
2023-12-20 22:10:19 +0530

    fixed rendering related bug causing errors

--- src/pages/productpage.js
+++ src/pages/productpage.js
@@ -5,6 +5,7 @@ import ProductImages from "../../components/product_info/ProductImages";
 import ProductInformation from "../../components/product_info/ProductInfo";
 import AddToCartButton from "../../components/product_info/Addtocart";
 import RelatedProducts from "../../components/product_info/RelatedProducts";
+
 const links = [
   { url: "/about", text: "About" },
   { url: "/contact", text: "Contact" },
@@ -12,23 +13,19 @@ const links = [
 ];
 
 const companyInfo = "Copyright © 2023 Snatkart";
+
 const ProductsPage = ({ product }) => {
   return (
     <div className="">
       <NavBar />
       <div className="product-page">
-        <ProductImages images={product.images} />
+        {product && product.images && <ProductImages images={product.images} />}
         <div className="product-details">
-          <ProductInformation
-            name={product.name}
-            description={product.description}
-            
-          />
-          {/* <ProductQuantity /> */}
-          <AddToCartButton  productLink={product.productLink}/>
+          {product && product.name && product.description && <ProductInformation name={product.name} description={product.description} />}
+          {product && product.productLink && <AddToCartButton productLink={product.productLink} />}
         </div>
       </div>
-      <RelatedProducts relatedProducts={product.relatedProducts} />
+      {product && product.relatedProducts && <RelatedProducts relatedProducts={product.relatedProducts} />}
       <Footer links={links} companyInfo={companyInfo} />
     </div>
   );
