349d5c82cba95247c59c81c424e5f584e15f9685
DeepanshuDabas03 <deepanshu21249@iiitd.ac.in>
2023-12-21 02:32:59 +0530

    all

--- next.config.js
+++ next.config.js
@@ -11,15 +11,11 @@ const nextConfig = {
         destination: "/product",
       },
       {
-        source: "/producta/:productName",
-        destination: "/producta",
-      },
-      {
         source: '/api/:path*',
         destination:
-          process.env.NODE_ENV === 'development'
+          process.env.NODE_ENV === 'development' 
             ? 'http://127.0.0.1:9010/:path*'
-            : '/api/:path*',
+            : './src/api/:path*',
       },
     ];
   },
